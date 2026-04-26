e0f93bd801c9521a0e087deb184ab0aa553db89c
jp2tb8ysmh-art <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>
2026-04-27 01:07:41 +0400

    Initial Commit

---
+++ StanfApp/StanfAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfAppApp.swift
+//  StanfApp
+//
+//  Created by Maxim Rybtsov on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct StanfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
